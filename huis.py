bdd61a44fd988e874c82a7daa7fc126a9eddb4b4
Sylvie <sylvielanghout@hotmail.com>
2018-04-17 15:00:37 +0200

    Maakt grid

--- huis.py
+++ huis.py
@@ -1,39 +1,51 @@
-Class Huis(object):
-	def__init__(self, x, y):
+
+class grid(object):
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+	def makegrid(self):
+		grid = []
+		row = []
+		for i in range(self.width):
+			row.append('0')
+		for i in range(self.height):
+			grid.append(row)
+		for i in range(len(grid)):
+			print(grid[i])
+
+class Huis(object):
+	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 		self.position = [x,y]
-	
-	def prijsverhoging(self):
-		return berekeningprijsverhoging
-	
-	@property
-	def cords(self):
-		return Huis.cords
 
-Class eensgezins(Huis):
-	def __init__(self):
-		super().init()
-		self.hoogte = 8
-		self.breedte = 8
-		self.prijs = 285.000
-		self.vrijstaand = 2
-		self.percentage = 1.03
+class woning(Huis):
+	def __init__(self, hoogte, breedte, prijs, vrijstaand, percentage):
+		super().__init__(x = 0, y = 0)
+		self.hoogte = hoogte
+		self.breedte = breedte
+		self.prijs = prijs
+		self.vrijstaand = vrijstaand
+		self.percentage = percentage
 
-Class bungalow(Huis):
-	def __init__(self):
-		super().init()
-		self.hoogte = 10
-		self.breedte = 7.5
-		self.prijs = 399.000
-		self.vrijstaand = 3
-		self.percentage = 1.04
+	def printhuis(self):
+		print("The house is {} by {} and worth {}".format(self.hoogte, self.breedte, self.prijs))
 
-Class maison(Huis):
-	def __init__(self):
-		super().init()
-		self.hoogte = 11
-		self.breedte = 10.5
-		self.prijs = 610.000
-		self.vrijstaand = 6
-		self.percentage = 1.06
+def build_house(grid, woning):
+	for i in range(grid.width):
+		for j in range(grid.height):
+			if [i][j] == '0':
+				for i in range(self.hoogte):
+					for j in range(self.breedte):
+						[i][j] = 'x'
+
+
+
+eensgezins = woning(2, 2, 285000, 2, 1.03)
+eensgezins.printhuis()
+
+land = grid(32, 36)
+land.makegrid()
+
+build_house(land, eensgezins)
