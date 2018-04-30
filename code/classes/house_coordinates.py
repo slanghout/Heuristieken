628559c2f36bf6abb926d56eec9578e89cf3be44
Sylvie <sylvielanghout@hotmail.com>
2018-04-30 20:56:56 +0200

    print huizen

--- code/classes/house_coordinates.py
+++ code/classes/house_coordinates.py
@@ -7,7 +7,7 @@ class grid(object):
 		self.width = width
 		self.height = height
 
-	def makegrid(self):
+	def makegrid(self, coordinatelist):
 		# make the figure
 		fig = plt.figure()
 
@@ -35,26 +35,24 @@ class grid(object):
 		plt.ylabel('height')
 		plt.title('Amstelhaege')
 		
-		# # set value of plots to 0
-		# for i in range(self.width):
-  #  	 		for j in range(self.height):
-  #  	 			plt.text(i, j, '0')
-
-		# dit zijn dus de coordinaten van het huis
-		# property?
-		# linken aan classes van de soorten huizen
-		# algoritme dat deze coordinaten bepaalt
-		# dit zijn dus de coordinaten van het huis
+		# make background grid green
 		x = [180, 180, 0, 0]
 		y = [0, 160, 160, 0]
-		# kleur aan huis aanpassen
-		ax.fill(x, y, "g") #per huis de legenda erachter
+		ax.fill(x, y, "g")
 		
-		x = [15, 15, 7, 7]
-		y = [7, 15, 15, 7]
-		ax.fill(x, y, "r") #per huis de legenda erachter
-		
-		
+		# iterate over coordinate list and select coordinates
+		for element in coordinatelist:
+			x = [element[2], element[2], element[0], element[0]]
+			y = [element[1], element[3], element[3], element[1]]
+				
+			# color coordinates coresponding color
+			if element[4] == 1:
+				ax.fill(x, y, "r")
+			elif element[4] == 2:
+				ax.fill(x, y, "y")
+			elif element[4] == 3:
+				ax.fill(x, y, "b")
+	
 		# plt.legend(loc=0)
 		plt.show()
 
@@ -68,7 +66,6 @@ class house(object):
 		self.price = price
 		self.space = space
 		self.percentage = percentage
-		# self.color = color
 		self.count = count
 
 	def coordinates_house(self):
@@ -76,73 +73,90 @@ class house(object):
 		down_y = self.y - self.height
 		right_x = self.x + self.width
 		up_y = self.y
-		house_coordinates = [left_x, up_y, right_x, down_y]
+		count = self.count
+		house_coordinates = [left_x, up_y, right_x, down_y, count]
 		if left_x < self.space:
 			print("does not fit left: {}".format(house_coordinates))
-		if right_x > (160 - self.space):
+		elif right_x > (160 - self.space):
 			print("does not fit right: {}".format(house_coordinates))
-		if up_y < self.space:
+		elif up_y > (160 - self.space):
 			print("does not fit on top: {}".format(house_coordinates))
-		if down_y > (160 - self.space):
+		elif down_y < self.space:
 		 	print("does not fit on the bottom: {}".format(house_coordinates))
 		else:
 			# print(house_coordinates)
 			return house_coordinates
 
+# define specifics for single house
 class single(house):
 	def __init__(self, coordinates):
 		super().__init__(x = coordinates[0], y=coordinates[1], height = 8,
 		 width = 8, price = 285000, space = 2, percentage = 1.03, count = 1)
 
+# define specifics for bungalow house
 class bungalow(house):
 	def __init__(self, coordinates):
 		super().__init__(x = coordinates[0], y=coordinates[1], height = 10,
 		 width = 7.5, price = 399000, space = 3, percentage = 1.04, count = 2)
 
+# define specifics for maison house
 class maison(house):
 	def __init__(self, coordinates):
 		super().__init__(x = coordinates[0], y=coordinates[1], height = 11,
 		 width = 10.5, price = 610000, space = 6, percentage = 1.06, count = 3)
   
-# Random coordinates generation
+# Random coordinate generation after grid boundaries are given
 def Randomizer(grid):
+
+	# max and min value of the coordinates are grid outliers
 	maxX = grid.width
 	maxY = grid.height
 	minX = 0
 	minY = 0
+	
+	# generate random x and y for left up corner
 	random_x = random.randint(minX, maxX)
 	random_y = random.randint(minY, maxY)
 	return [random_x, random_y]
-#  als coordinaten leeg zijn; kijken of @property allemaal leeg zijn: dan huis planten
 
+# set height and width of the land
 land = grid(180, 160)
-land.makegrid() 
 
+# create empty coordinate list
 coordinate_list = []
+
+# create 10 random coordinates
 for houses in range(10):
 	cord = Randomizer(land)
-	house = maison(cord)
+
+	# create either single, bungalow or maison
+	housenr = random.randint(1, 3)
+	if housenr == 1:
+		build = single
+	elif housenr == 2:
+		build = bungalow
+	elif housenr == 3:
+		build = maison
+	house = build(cord)
+	
+	# check if the coordinates are overlapping
 	this_cord = house.coordinates_house()
 	if this_cord != None:
 		if len(coordinate_list) > 0:
 			coordinate_list.append(this_cord)
+			# TODO WERK NIET
 			for cords in coordinate_list:
-				if this_cord[0]<cords[0] and this_cord[0]>cords[2] and this_cord[1]>cords[1] and this_cord[1]<cords[3]:
+				if (this_cord[0]>cords[0] or this_cord[0]==cords[0]) and (this_cord[0]<cords[2] or this_cord[0]==cords[2]) and (this_cord[1]<cords[1] or this_cord[1]==cords[1]) and (this_cord[1]>cords[3] or this_cord[1]==cords[3]):
 					print("overlap")
 				else:
-					coordinate_list.append(this_cord)
+					print("would fit")
 		else:
 			coordinate_list.append(this_cord)
 			
 
 print(coordinate_list)
-	
-	# new_house = single(Randomizer(land))
 
+# make a grid of the coordinates in coordinate list
+land.makegrid(coordinate_list) 
 
-# def build_house(grid, woning):
-# 	#  iterate over the width of the grid
-# 	# if there is an empty block, check if there are enough empty blocks to make house
-# 	# if true check if this is the case for height of the house
-# 	# if true, plot the house, change value of blocks to 1, 2, 3
 
