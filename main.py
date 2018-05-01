c0dd52dc89200f899753a095949c8d3d6ca32152
pernilledeijlen <pernille_deijlen@live.nl>
2018-05-01 10:45:01 +0200

    new

---
+++ main.py
@@ -0,0 +1,18 @@
+# Authors: Dewi Mooij, Sylvie Langhout & Pernille Deijlen
+# Amstelhaege
+
+
+from import *
+from import *
+from water import *
+
+if __name__ == "__main__"
+
+	# placing the houses and water randomly
+
+	# visualizing data
+
+	# Hill Climbing Algorithm
+
+	# Simulated Annealing Algorithm
+
